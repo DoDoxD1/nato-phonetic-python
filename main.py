b7a4e7bdf98c2f165bc883388422f28e8b72becd
= <=>
2024-08-14 21:11:21 +0530

    nato phonetic dictionary

---
+++ main.py
@@ -0,0 +1,23 @@
+import pandas
+
+# reading dataframe
+df = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+# creating lists out of dataframe
+letters = []
+codes = []
+for (key, value) in df.iterrows():
+    letters.append(value.letter)
+    codes.append(value.code)
+
+# using dictionary comprehension to convert dataframe into dictionary
+data = {key: value for (key, value) in zip(letters, codes)}
+
+# getting user input
+input_word = input("Enter a word: ").upper()
+output = []
+for letter in input_word:
+    # creating a list of words using dictionary
+    output.append(data[letter])
+
+print(output)
