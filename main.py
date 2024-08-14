db4d627354e9f8cac4970e133cfd1dd6745b2419
= <=>
2024-08-14 21:16:49 +0530

    improved code

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ import pandas
 # reading dataframe
 df = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-# creating lists out of dataframe
-letters = []
-codes = []
-for (key, value) in df.iterrows():
-    letters.append(value.letter)
-    codes.append(value.code)
+# # creating lists out of dataframe
+# letters = []
+# codes = []
+# for (key, value) in df.iterrows():
+#     letters.append(value.letter)
+#     codes.append(value.code)
+#
+# # using dictionary comprehension to convert dataframe into dictionary
+# data = {key: value for (key, value) in zip(letters, codes)}
 
-# using dictionary comprehension to convert dataframe into dictionary
-data = {key: value for (key, value) in zip(letters, codes)}
+# OR
+
+data = {value.letter: value.code for (index, value) in df.iterrows()}
 
 # getting user input
 input_word = input("Enter a word: ").upper()
